926f4cb7065957ab70a140d1139fc65effccb43d
Mohammed Abdulkereem <mohammed-io@outlook.com>
2019-02-14 11:14:54 +0300

    classList and className

--- dom.js
+++ dom.js
@@ -1,40 +1,54 @@
-const appUl = document.querySelector('#app ul');
+const appUl = document.querySelector("#app ul");
 
-const appendTodoToUl = function(texts, ul) {
-    [texts]
-        .flat()
-        .map((text, i) => {
-            const li = document.createElement('li')
-            
-            const a = document.createElement('a');
-            a.innerHTML = text
+const appendTodoToUl = function(texts, ul, noUnderline = false) {
+  if (noUnderline) {
+    // add the class of no-underline and remove with-undeline to ul
+    // ul.classList.remove('with-underline')
+    // ul.classList.add('no-underline')
 
-            li.appendChild(a);
+    // or replace
+    ul.classList.replace('with-underline', 'no-underline')
 
-            a.setAttribute('href', `http://${text}.com`);
-
-            a.style.fontSize = `${15 + (i * 5)}px`;
-
-            a.style.backgroundColor = `rgba(${i * 90 % 256}, ${i * 60 % 256}, ${i * 80 % 256}, .5)`;
-
-            if (text.length <= 4) {
-                li.setAttribute('class', 'green-text');
-            } else if (text.length < 8) {
-                li.setAttribute('class', 'yello-text');
-            } else {
-                li.setAttribute('class', 'red-text');
-            }
-
-            return li;
-        }).forEach(li => {
-            ul.appendChild(li)
-        })
+    // logs the classes of ul
+    console.log(ul.getAttribute('class'))
+    console.log(ul.className)
 }
 
-appendTodoToUl([
-    'Mohammed',
-    'Ali',
-    'Shakhawan',
-    'Najiba',
-    'Omar'
-], appUl)
+  [texts]
+    .flat()
+    .filter(Boolean)
+    .map((text, i) => {
+      const li = document.createElement("li");
+
+      const a = document.createElement("a");
+      a.innerHTML = text;
+
+      li.appendChild(a);
+
+      a.setAttribute("href", `http://${text.toLowerCase()}.com`);
+
+      a.style.fontSize = `${15 + i * 5}px`;
+
+      a.style.backgroundColor = `rgba(${(i * 90) % 256}, 
+                ${(i * 60) % 256}, ${(i * 80) % 256}, .5)`;
+
+      if (text.length <= 4) {
+        li.setAttribute("class", "green-text");
+      } else if (text.length < 8) {
+        li.setAttribute("class", "yello-text");
+      } else {
+        li.setAttribute("class", "red-text");
+      }
+
+      return li;
+    })
+    .forEach(li => {
+      ul.appendChild(li);
+    });
+};
+
+appendTodoToUl(
+  ["Mohammed", "Ali", "Shakhawan", "Najiba", null, "", undefined, "Omar"],
+  appUl,
+  true
+);
