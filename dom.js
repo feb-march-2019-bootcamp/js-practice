b3a17bddfec628892461b8ae64619cbda591c2a9
Mohammed Abdulkereem <mohammed-io@outlook.com>
2019-02-14 10:05:45 +0300

    setAttribute and style

--- dom.js
+++ dom.js
@@ -3,9 +3,28 @@ const appUl = document.querySelector('#app ul');
 const appendTodoToUl = function(texts, ul) {
     [texts]
         .flat()
-        .map(text => {
+        .map((text, i) => {
             const li = document.createElement('li')
-            li.innerHTML = text
+            
+            const a = document.createElement('a');
+            a.innerHTML = text
+
+            li.appendChild(a);
+
+            a.setAttribute('href', `http://${text}.com`);
+
+            a.style.fontSize = `${15 + (i * 5)}px`;
+
+            a.style.backgroundColor = `rgba(${i * 90 % 256}, ${i * 60 % 256}, ${i * 80 % 256}, .5)`;
+
+            if (text.length <= 4) {
+                li.setAttribute('class', 'green-text');
+            } else if (text.length < 8) {
+                li.setAttribute('class', 'yello-text');
+            } else {
+                li.setAttribute('class', 'red-text');
+            }
+
             return li;
         }).forEach(li => {
             ul.appendChild(li)
@@ -13,8 +32,9 @@ const appendTodoToUl = function(texts, ul) {
 }
 
 appendTodoToUl([
-    'First LI',
-    'second li',
-    'third li',
-    'fourth li'
+    'Mohammed',
+    'Ali',
+    'Shakhawan',
+    'Najiba',
+    'Omar'
 ], appUl)
