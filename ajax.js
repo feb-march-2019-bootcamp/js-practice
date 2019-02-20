edd7c3eb85ce21d86f6449e602b574eb79676b3f
Mohammed Abdulkereem <mohammed-io@outlook.com>
2019-02-20 09:53:05 +0300

    Used axios

--- ajax.js
+++ ajax.js
@@ -1,3 +1,11 @@
+import Axios from 'axios';
+
+const newPost = {
+    userId: 1,
+    title: "This is title",
+    body: "This is body"
+  };
+
 // the old
 const xhr = new XMLHttpRequest();
 
@@ -7,19 +15,29 @@ xhr.addEventListener("load", () => {
   console.log(JSON.parse(xhr.responseText));
 });
 
-const newPost = {
-  userId: 1,
-  title:
-    "This is title",
-  body:
-    "This is body"
-};
+xhr.setRequestHeader('Content-Type', 'application/json');
 
-xhr.send(newPost);
-
+xhr.send(JSON.stringify(newPost));
 
 // The modern fetch api
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(e => console.log(e))
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(x => x.json())
+//     .then(x => console.log(x))
+//     .catch(x => console.log(x))
+
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "post", // or put, patch, delete
+  body: JSON.stringify(newPost),
+  headers: {
+      'Content-Type': 'application/json'
+  }
+})
+.then(x => x.json())
+.then(console.log)
+.catch(console.log)
+
+
+// The axios way
+Axios.post('https://jsonplaceholder.typicode.com/posts', newPost)
+    .then(res => console.log(res.data))
+    .catch(console.log);
